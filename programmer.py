254198b1e3b1c584dfeb43aac90dc969c2fa47e8
IntrovertCoderr <mrinmoydeka82@gmail.com>
2022-05-19 23:51:18 +0530

    reading cell for data

--- programmer.py
+++ programmer.py
@@ -13,7 +13,7 @@ sheet = wb.sheet_by_index(0)
 arr =[]
 # For row 0 and column 0
 for x in range(0,160):
-    arr.append((sheet.cell_value(x, 5)))
+    arr.append((sheet.cell_value(x, 4)))
 
 print (arr)
 
